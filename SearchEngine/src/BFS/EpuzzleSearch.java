dae684f646959ebcc2613341525671e2bab52a4c
SashaSaw <asaw1@sheffield.ac.uk>
2022-05-07 11:05:10 +0100

    changed variables storing the configurations of puzzles to 2d arrays in EpuzzleSearch.java

--- SearchEngine/src/BFS/EpuzzleSearch.java
+++ SearchEngine/src/BFS/EpuzzleSearch.java
@@ -2,22 +2,22 @@ package BFS;
 
 public class EpuzzleSearch extends Search{
 
-	private int[] puzzleConfig; // configuration of the puzzle in current state
-	private int[] target; // the configuration of the target puzzle state
+	private int[][] puzzleConfig = new int [3][3]; // configuration of the puzzle in current state
+	private int[][] target = new int [3][3]; // the configuration of the target puzzle state
 	
-	public EpuzzleSearch (int[] puzzleC, int[] tar) {
+	public EpuzzleSearch (int[][] puzzleC, int[][] tar) {
 		
 		puzzleConfig = puzzleC;
 		target = tar;
 	}
 	
 	// accessor for puzzle configuration
-	public int[] getPuzzleConfig() {
+	public int[][] getPuzzleConfig() {
 		return puzzleConfig;
 	}
 	
 	//accessor for target puzzle configuration
-	public int[] getTarget() {
+	public int[][] getTarget() {
 		return target;
 	}
 	
--- SearchEngine/src/BFS/RunEpuzzleBFS.java
+++ SearchEngine/src/BFS/RunEpuzzleBFS.java
@@ -4,6 +4,19 @@ public class RunEpuzzleBFS {
 
 	public static void main(String[] arg) {
 		
+		int[][] puzzleConfig = {{1,0,3},
+								{4,2,6},
+								{7,5,8}};
+		
+		int[][] target = {{1,2,3},
+				{4,2,6},
+				{7,5,8}};
+		
+		EpuzzleSearch searcher = new EpuzzleSearch(puzzleConfig, target);
+		SearchState initState = (SearchState) new EpuzzleState();
+		
+		String resb = searcher.runSearch(initState, "breadFirst");
+		System.out.println(resb);
 		
 	}
 }
